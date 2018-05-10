e31ffbd6f613284b887e11222202b4071834cf55
LALIT KUMAR BEHERA <leo@leo.local>
2018-05-10 12:17:33 +0530

    login page added

---
+++ app/src/main/java/com/kalinga/apps/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.kalinga.apps
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
